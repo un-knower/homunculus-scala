4c98967d56f2223f03641fcc17dd0b65ace6c3e1
zhangchengyu <zhangchengyu@dianwoba.com>
2018-04-18 22:17:36 +0800

    structured

--- src/main/scala/com/homunculus/common/OrderAbandonLogType.scala
+++
@@ -1,9 +0,0 @@
-package com.homunculus.common
-
-/**
- * OrderAbandonLogType
- *
- * @author Jade
- */
-case class OrderAbandonLogType(val id: java.lang.Long,
-                               val city_id: java.lang.Long)
--- src/main/scala/com/homunculus/spark/StructuredStreaming2ESTest.scala
+++ src/main/scala/com/homunculus/spark/StructuredStreaming2ESTest.scala
@@ -1,9 +1,9 @@
 package com.homunculus.spark
 
-import java.util
+import java.text.SimpleDateFormat
 
-import com.google.gson.Gson
-import com.homunculus.common.{CanalModel, OrderAbandonLogType}
+import com.google.gson.{Gson, GsonBuilder}
+import com.homunculus.common.CanalModel
 import org.apache.spark.sql.streaming.{OutputMode, Trigger}
 import org.apache.spark.sql.{Row, SparkSession}
 
@@ -23,6 +23,7 @@ object StructuredStreaming2ESTest {
 
     import spark.implicits._
 
+
     val query = spark
       .readStream
       .format("kafka")
@@ -33,15 +34,15 @@ object StructuredStreaming2ESTest {
       .load()
       .selectExpr("CAST(key AS STRING)", "CAST(value AS STRING)")
       .map(($: Row) => {
-        print($)
-        val canalModel = (new Gson).fromJson($.getString(1), classOf[CanalModel])
-        val dataMap = processFieldsTransToES(canalModel)
-        val id: java.lang.Long = dataMap.get("id").get.asInstanceOf[Long]
-        val city_id: java.lang.Long = if (dataMap.get("city_id") != None) dataMap.get("city_id").get.asInstanceOf[Long] else null
-
-        val a = new OrderAbandonLogType(id, city_id)
-
+        val gson: Gson = new GsonBuilder().serializeNulls.create
+        val canalModel = gson.fromJson($.getString(1), classOf[CanalModel])
+        val dmlType = canalModel.dmlType.toLowerCase
+        val column = if (dmlType.equals("delete")) canalModel.beforeColumn else canalModel.afterColumn
+        val esModel = gson.fromJson(gson.toJson(column), classOf[OrderAbandonLogType]).init
+        (dmlType, esModel)
       })
+      .filter($ => !(java.util.Objects.equals($._1, "delete")))
+      .map($ => $._2)
 
 
     println(s"query.schema = ${query.schema}")
@@ -59,19 +60,49 @@ object StructuredStreaming2ESTest {
       .option("es.write.operation", "upsert")
       .option("es.update.retry.on.conflict", "10")
       .option("es.spark.dataframe.write.null", "true")
-      .option("es.resource.write", "bigdata-{city_id}/structured_streaming_test")
+      .option("es.mapping.exclude", "index_pt")
+      .option("es.resource.write", "ss_order_abandon_log-{index_pt}/ss_order_abandon_log")
       .queryName("ElasticSink")
       .start
       .awaitTermination()
 
   }
 
+}
 
-  def processFieldsTransToES(canalModel: CanalModel, clazz: Class[_]): _ = {
-    val dmlType = canalModel.dmlType.toLowerCase
-    val column = if (dmlType.equals("delete")) canalModel.beforeColumn else canalModel.afterColumn
-    val gson: Gson = new Gson
-    gson.fromJson(gson.toJson(column), clazz)
+
+/**
+ *
+ * 日期类型用var 用于空串转null (kafka过来就是空串)
+ *
+ * @param index_pt 动态索引指定字段
+ */
+case class OrderAbandonLogType(val id: java.lang.Long,
+                               val city_id: java.lang.Integer,
+                               val order_id: java.lang.Long,
+                               val rider_id: java.lang.Integer,
+                               val reason: java.lang.Integer,
+                               val why: java.lang.String,
+                               val order_status: java.lang.Byte,
+                               var switch_tm: java.lang.String,
+                               var dispatch_tm: java.lang.String,
+                               var arrive_tm: java.lang.String,
+                               val dispatch_mode: java.lang.Byte,
+                               val source: java.lang.Byte,
+                               val shardx: java.lang.Integer,
+                               val shardz: java.lang.Integer,
+                               val feature: java.lang.String,
+                               var index_pt: java.lang.String) {
+  def init(): OrderAbandonLogType = {
+    if (java.util.Objects.equals(dispatch_tm, "")) {
+      this.dispatch_tm = null
+    }
+    if (java.util.Objects.equals(arrive_tm, "")) {
+      this.arrive_tm = null
+    }
+    val sdFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss")
+    val ptFormat = new SimpleDateFormat("yyyyMM")
+    this.index_pt = ptFormat.format(sdFormat.parse(this.switch_tm))
+    this
   }
-
 }
